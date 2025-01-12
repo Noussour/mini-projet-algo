c88d0023fc6578218cbc9164f204f3ece69b44ba
edaywalid <becharwalid9@gmail.com>
2025-01-12 21:56:59 +0100

    added visualize results script

---
+++ adel-walid/code/scripts/visualize_result.py
@@ -0,0 +1,15 @@
+# scripts/visualize_results.py
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/result.csv")
+for algo in df["Algorithm"].unique():
+    subset = df[df["Algorithm"] == algo]
+    plt.plot(subset["Array Size"], subset["Execution Time"], label=algo)
+
+plt.xlabel("Array Size")
+plt.ylabel("Execution Time (s)")
+plt.title("Sorting Algorithm Performance")
+plt.legend()
+plt.show()
+
