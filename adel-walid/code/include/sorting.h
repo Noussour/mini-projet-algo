d2fbcee4bc9a6c94e1acd43f16259f6b0bf5c5a4
Adel2411 <hadjarabadel.2411@gmail.com>
2025-01-12 04:50:08 +0100

    Added radix sort implementation

--- adel-walid/code/include/sorting.h
+++ adel-walid/code/include/sorting.h
@@ -9,7 +9,8 @@ void bubble_sort(int arr[], int n);
 
 void gnome_sort(int arr[], int n);
 
-void radix_sort(int arr[], int n);
+void sort_aux(int arr[], int n, int i);
+void radix_sort(int arr[], int n, int k);
 
 int partition(int arr[], int low, int high);
 void quick_sort(int arr[], int low, int high);
--- adel-walid/code/src/radix_sort.c
+++ adel-walid/code/src/radix_sort.c
@@ -1,1 +1,31 @@
 #include "../include/sorting.h"
+#include "../include/utils.h"
+
+// Uses counting sort algorithm which runs in linear time O(n)
+void sort_aux(int arr[], int n, int i) {
+  int *output = (int *)malloc(n * sizeof(int));
+  int count[10] = {0};
+
+  for (int j = 0; j < n; j++) {
+    count[key(arr[j], i)]++;
+  }
+  for (int j = 1; j < 10; j++) {
+    count[j] += count[j - 1];
+  }
+  for (int j = n - 1; j >= 0; j--) {
+    output[count[key(arr[j], i)] - 1] = arr[j];
+    count[key(arr[j], i)]--;
+  }
+  for (int j = 0; j < n; j++) {
+    arr[j] = output[j];
+  }
+
+  free(output);
+}
+
+void radix_sort(int arr[], int n, int k) {
+  // k represents the maximum number of digits
+  for (int i = 0; i < k; i++) {
+    sort_aux(arr, n, i);
+  }
+}
