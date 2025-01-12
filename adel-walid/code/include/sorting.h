e2fd273bda0f8de68d199bebacdbe8e4bcfd18ed
Adel2411 <hadjarabadel.2411@gmail.com>
2025-01-12 04:19:34 +0100

    Added quick sort implementation

--- adel-walid/code/include/sorting.h
+++ adel-walid/code/include/sorting.h
@@ -6,10 +6,15 @@
 #include <time.h>
 
 void bubble_sort(int arr[], int n);
+
 void gnome_sort(int arr[], int n);
-void quick_sort(int arr[], int n);
+
+void radix_sort(int arr[], int n);
+
+int partition(int arr[], int low, int high);
+void quick_sort(int arr[], int low, int high);
+
 void heapify(int arr[], int n, int i);
 void heap_sort(int arr[], int n);
-void radix_sort(int arr[], int n);
 
 #endif // SORTING_H
--- adel-walid/code/src/quick_sort.c
+++ adel-walid/code/src/quick_sort.c
@@ -1,1 +1,35 @@
 #include "../include/sorting.h"
+#include "../include/utils.h"
+
+int partition(int tab[], int p, int r) {
+  int eltPivot = tab[p];
+  int i = p;
+  int j = r;
+
+  do {
+    // Find element from right that's smaller than pivot
+    while (j >= p && tab[j] > eltPivot) {
+      j--;
+    }
+    // Find element from left that's larger than pivot
+    while (i <= r && tab[i] <= eltPivot) {
+      i++;
+    }
+    // Swap elements if indexes haven't crossed
+    if (j > i) {
+      swap(&tab[i], &tab[j]);
+      j--;
+      i++;
+    }
+  } while (j > i);
+
+  return j;
+}
+
+void quick_sort(int tab[], int p, int r) {
+  if (p < r) {
+    int q = partition(tab, p, r);
+    quick_sort(tab, p, q);
+    quick_sort(tab, q + 1, r);
+  }
+}
