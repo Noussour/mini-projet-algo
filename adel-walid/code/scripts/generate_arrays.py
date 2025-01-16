ffda0eef436969d60a7c69a390421e09e49ab3d7
Adel2411 <hadjarabadel.2411@gmail.com>
2025-01-16 21:07:14 +0100

    Handling file errors and changing some data to the final plot and to radix algorithm

--- adel-walid/code/scripts/generate_arrays.py
+++ adel-walid/code/scripts/generate_arrays.py
@@ -1,13 +1,24 @@
 import random
 
+
 def generate_arrays(file_path, sizes, max_value):
-    with open(file_path, "w") as f:
-        for size in sizes:
-            array = [random.randint(0, max_value) for _ in range(size)]
-            f.write(" ".join(map(str, array)) + "\n")
+    """Generate arrays of random integers and write them to a file.
+
+    Args:
+        file_path (str): The path to the output file.
+        sizes (list of int): A list of sizes for the arrays to generate.
+        max_value (int): The maximum value for the random integers.
+    """
+    try:
+        with open(file_path, "w") as f:
+            for size in sizes:
+                array = [random.randint(0, max_value) for _ in range(size)]
+                f.write(" ".join(map(str, array)) + "\n")
+    except IOError as e:
+        print(f"Error writing to file {file_path}: {e}")
+
 
 if __name__ == "__main__":
-    sizes = [10, 100, 500, 1000]  # Arrays sizes
-    max_value = 1000             # Maximum value in arrays
+    sizes = [100, 300, 500, 800, 1000, 1300, 1500, 2000, 2500]  # Array sizes
+    max_value = 1000  # Maximum value in arrays
     generate_arrays("data/input_arrays.txt", sizes, max_value)
-
